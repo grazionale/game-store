b18c153b8875f10d47da6b1fd4406ad622b6852c
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2020-11-04 15:45:20 -0300

    Possibilitando que o devise token auth aceite name no cadastro de usuário

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
+
+# if: :devise_controller? serve para carregar o código configure_permitted_parameters 
+# somenete no devise_controller
+
+# Por padrão, o devise aceita apenas email, password e 
+# password_confirmation na criação de um novo usuário
+# por isso, através da função configure_permitted_parameters
+# definimos que ele também aceitará o campo name
