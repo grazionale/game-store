4b19ff55fed9a96a119ba23fa0ae9a962a6b6525
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2020-11-18 09:13:09 -0300

    Refatorando render_error do api_controller para ser um concern e poder receber um partial de modelo de erro pelo jbuilder

--- app/controllers/admin/v1/api_controller.rb
+++ app/controllers/admin/v1/api_controller.rb
@@ -3,6 +3,8 @@ module Admin::V1
     # chamaremos de ForbiddenAccess e é ela que vamos lançar quando o erro de acesso ocorrer
     class ForbiddenAccess < StandardError; end
     include Authenticatable # concerm criado para forçar que o usuário esteja autenticado
+    include SimpleErrorRenderable
+    self.simple_error_partial = "shared/simple_error"
     
     # antes de toda chamada aos endpoints do admin que herdem de ApiController, será feito
     # uma verificação para saber se o usuário é admin
@@ -11,13 +13,6 @@ module Admin::V1
     # toda vez que houver um erro(recusa) na classe ForbiddenAccess será executado esse bloco
     rescue_from ForbiddenAccess do 
       render_error(message: "Forbidden access", status: :forbidden)
-    end
-
-    def render_error(message: nil, fields: nil, status: :unprocessable_entity)
-      errors = {}
-      errors['fields'] = fields if fields.present?
-      errors['message'] = message if message.present?
-      render json: { errors: errors }, status: status
     end
 
     private
