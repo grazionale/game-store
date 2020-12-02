3ebc85c865f4c479b2cc36d715ac23fcc597c6c9
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2020-12-02 08:59:12 -0300

    Refatorando system_requirements_controller para novo retorno de paginação

--- app/controllers/admin/v1/system_requirements_controller.rb
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -3,8 +3,9 @@ module Admin::V1
     before_action :load_system_requirement, only: [:update, :destroy]
 
     def index
-      @system_requirements = SystemRequirement.all
-    end
+      @loading_service = Admin::ModelLoadingService.new(SystemRequirement.all, searchable_params)
+      @loading_service.call
+    end  
 
     def create
       @system_requirement = SystemRequirement.new
@@ -24,6 +25,10 @@ module Admin::V1
     end       
 
     private
+
+    def searchable_params
+      params.permit({ search: :name }, { order: {} }, :page, :length)
+    end
 
     def load_system_requirement
       @system_requirement = SystemRequirement.find(params[:id])
