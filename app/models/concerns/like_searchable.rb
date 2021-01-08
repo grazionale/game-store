f65c855558e1c6d25bcd5520a8fc61a9a7cf2f89
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2021-01-08 09:04:54 -0300

    Criando like_searchable concern

---
+++ app/models/concerns/like_searchable.rb
@@ -0,0 +1,11 @@
+module LikeSearchable
+  extend ActiveSupport::Concern
+
+  included do
+    # scope são consultas comumente usadas que podem ser referenciadas como chamadas de métodos
+    # nos objetos ou em modelo de associações. Ex: Category.like('nome_do_campo', 'nome_que_deseja_encontrar')
+    scope :like, -> (key, value) do
+      self.where("#{key} ILIKE ?", "%#{value}%")
+    end
+  end  
+end
