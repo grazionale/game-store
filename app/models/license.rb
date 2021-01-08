1589547f3dbcfb29bfe2a1643dc85e9df63ba3a8
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2021-01-08 08:53:38 -0300

    Adicionando concern paginatable em Licenses

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,9 +1,11 @@
 class License < ApplicationRecord
+  include Paginatable
+
   belongs_to :game
 
   validates :key, presence: true, uniqueness: { case_sensitive: false, scope: :platform }
   validates :platform, :status, presence: true
-  
+
   enum platform: { steam: 1, battle_net: 2, origin: 3 }
   enum status: { available: 1, in_use: 2, inactive: 3 }
 end
