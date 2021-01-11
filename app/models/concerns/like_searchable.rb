ce041fd73cac60620702e8f05281bf85383e7168
Gabriel Grazionale <gabriel.grazionale@portalsolar.com>
2021-01-11 08:11:00 -0300

    Refatorando ILIKE

--- app/models/concerns/like_searchable.rb
+++ app/models/concerns/like_searchable.rb
@@ -5,7 +5,7 @@ module LikeSearchable
     # scope são consultas comumente usadas que podem ser referenciadas como chamadas de métodos
     # nos objetos ou em modelo de associações. Ex: Category.like('nome_do_campo', 'nome_que_deseja_encontrar')
     scope :like, -> (key, value) do
-      self.where("#{key} ILIKE ?", "%#{value}%")
+      self.where(self.arel_table[key].matches("%#{value}%"))
     end
   end  
 end
